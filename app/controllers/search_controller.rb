2e7b28079a220c17fa025b0b3d3c0c6e7f197089
Cydnee Owens <cks410@yahoo.com>
2025-05-07 16:56:20 -0700

    remove solutions

--- app/controllers/search_controller.rb
+++
@@ -1,5 +0,0 @@
-class SearchController < ApplicationController
-  def index
-    @members = MembersFacade.members_by_nation('Fire+Nation') if params[:nation] == 'fire_nation'
-  end
-end
--- app/facades/members_facade.rb
+++
@@ -1,7 +0,0 @@
-class MembersFacade
-  def self.members_by_nation(nation)
-    url     = "/api/v1/characters?affiliation=#{nation}&perPage=150&page=1"
-    members = MembersService.call_db(url)
-    members.map { |member| MembersInfo.new(member) }
-  end
-end
